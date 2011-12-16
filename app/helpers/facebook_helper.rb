bed58442d359f74fcc769e67cd6c433658aeb8ee
Tim Linquist <tim.linquist@gmail.com>
2011-12-16 02:50:49 -0800

    Add extended permission for reading feeds
    Facebook only allows 3rd party apps to read friends' feeds of logged in
    user when extended permissions are enabled for the app on the app's
    preferences and in the login process. Add read_stream perm to fb login
    for full access to feeds.

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -1,7 +1,7 @@
 module FacebookHelper
   def facebook_login_button(size='large')
     content_tag("fb:login-button", nil , {
-      :scope => 'user_likes, friends_likes',
+      :scope => 'user_likes, friends_likes, read_stream',
       :id => "fb_login",
       :autologoutlink => 'true',
       :size => size,
